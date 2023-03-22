7e7e1a90b1c0e57368f38acd922135a5ad2cf744
dj-soneka <aclecioscruz@gmail.com>
2023-03-22 00:09:17 -0300

    feature: configuração do bd h2, profile de test, dependencias do JPA e H2 database

--- src/main/java/com/acleciocruz/api/entities/User.java
+++ src/main/java/com/acleciocruz/api/entities/User.java
@@ -1,5 +1,6 @@
 package com.acleciocruz.api.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
@@ -9,8 +10,12 @@ import java.io.Serializable;
 @AllArgsConstructor
 @EqualsAndHashCode
 @Builder
+@Entity
+@Table(name = "TB_USER")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
